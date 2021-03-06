2979fd5a9869c6ffc8546d0a45251f570e2864e3
yesseri <henrik.zenkert@gmail.com>
2021-03-06 21:19:33 +0100

    started server

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
-import React, { Component, useState } from 'react';
-import Game from "./pages/Game";
-import Chess from 'chess.js'
+import React, { useEffect } from 'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+// import Game from "./pages/Game";
 import './App.css';
+const client = new W3CWebSocket('ws://127.0.0.1:5000');
 
 function App() {
+  useEffect(() => {
+    console.log("useeffect is run")
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+    client.onmessage = (message) => {
+      console.log(message);
+    };
+ 
+  }, []) 
   return (
-    <Game />
+
+      <div>
+        Practical Intro To WebSockets.
+      </div>
+    // <Game />
   );
 }
+
 
 export default App;
