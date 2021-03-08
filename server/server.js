48f027c8f9195c51a48747e2340f65fcf6768033
YesSeri <henrik.zenkert@gmail.com>
2021-03-08 22:11:04 +0100

    Almost working multiplayer

--- server/server.js
+++ server/server.js
@@ -1,27 +1,57 @@
-const webSocketsServerPort = 5000;
-const webSocketServer = require('websocket').server;
-const http = require('http');
-// Spinning the http server and the websocket server.
-const server = http.createServer();
-server.listen(webSocketsServerPort);
-const wsServer = new webSocketServer({
-  httpServer: server
+const { v4: UniqID } = require('uuid');
+const express = require('express')
+const http = require('http')
+const WebSocket = require('ws');
+const { Chess } = require('chess.js')
+
+const app = express();
+
+//initialize a simple http server
+const server = http.createServer(app);
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+const teams = { white: [], black: [] }
+
+const chess = new Chess()
+
+// Everytime I send something I should have a payload and metadata in the object sent. 
+wss.on('connection', (ws) => {
+  // Adds a unique id to each client
+  ws.id = UniqID();
+  addToTeam(ws);
+  ws.send(JSON.stringify({ subject: "greeting", metadata: new Date(), payload: { message: 'Welcome to the server, new client!' } }));
+  ws.on('message', (message) => {
+    if (message === 'joinedGame') {
+      ws.send(JSON.stringify({ subject: 'fetchedCurrentBoard', payload: { fen: chess.fen() } }))
+    } else {
+      console.log(`Hello, you sent -> ${message}`);
+    }
+  });
+  ws.on('close', () => {
+    removeFromTeam(ws)
+  });
 });
 
-// I'm maintaining all active connections in this object
-const clients = {};
+function addToTeam(ws) {
+  if (teams.white.length < teams.black.length) {
+    teams.white.push(ws.id);
+    ws.team = 'w'
+  } else {
+    teams.black.push(ws.id);
+    ws.team = 'b'
+  }
+}
+function removeFromTeam(ws) {
+  for (team in teams) {
+    const idx = team.indexOf(ws.id);
+    if (idx !== -1) {
+      array.splice(index, 1);
+    }
+  }
+}
 
-// This code generates unique userid for everyuser.
-const getUniqueID = () => {
-  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-  return s4() + s4() + '-' + s4();
-};
-
-wsServer.on('request', function (request) {
-  var userID = getUniqueID();
-  console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
-  // You can rewrite this part of the code to accept only the requests from allowed origin
-  const connection = request.accept(null, request.origin);
-  clients[userID] = connection;
-  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients))
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server started on port ${server.address().port}.`);
 });
