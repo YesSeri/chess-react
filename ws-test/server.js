aa7c44a6626213faea9780d1eaf3e808069a62d0
YesSeri <henrik.zenkert@gmail.com>
2021-03-11 21:27:51 +0100

    got a test server up and running with Stockfish

--- ws-test/server.js
+++ ws-test/server.js
@@ -1,19 +1,39 @@
 const express = require('express')
-const http = require('http')
-const stockfish = require('stockfish')
+const Engine = require('node-uci').Engine
 
-const { Worker, isMainThread, parentPort } = require('worker_threads');
+var path = require('path');
 
-const app = express();
 
-//initialize a simple http server
-const server = http.createServer(app);
 
-// var stockfish = new Worker("stockfish.js");
-// console.log(stockfish.sendMessage());
-var a = STOCKFISH();
+async function test_engine() {
+    const p = path.join(__dirname, '..', 'stockfish_13_win_x64_avx2.exe');
+    const engine = new Engine(p)
+    await engine.init()
+    await engine.setoption('MultiPV', '4')
+    await engine.isready()
+    await engine.position('r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3')
+    console.log('engine ready', engine.id, engine.options)
+    const result = await engine.go({depth:15})
+    console.log('result', result)
+    await engine.quit()
 
-//start our server
+}
+
+
+function test_engine_promises() {
+    const p = path.join(__dirname, '..', 'stockfish_13_win_x64_avx2.exe');
+    const engine = new Engine(p)
+    engine
+        .chain()
+        .init()
+        .setoption('MultiPV', 3)
+        .position('r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3')
+        .go({ depth: 15 })
+        .then(result => {
+            console.log(result)
+        })
+}
+test_engine();
 // server.listen(process.env.PORT || 5000, () => {
 //     console.log(`Server started on port ${server.address().port} :)`);
 // });
