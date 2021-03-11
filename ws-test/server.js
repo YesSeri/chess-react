4175c222770cfbe538e9e70eb6f62f9f42252cc4
YesSeri <henrik.zenkert@gmail.com>
2021-03-11 10:42:58 +0100

    commit

--- ws-test/server.js
+++ ws-test/server.js
@@ -1,39 +1,19 @@
 const express = require('express')
 const http = require('http')
-const WebSocket = require('ws');
-const path = require('path');
+const stockfish = require('stockfish')
+
+const { Worker, isMainThread, parentPort } = require('worker_threads');
 
 const app = express();
 
 //initialize a simple http server
 const server = http.createServer(app);
 
-//initialize the WebSocket server instance
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (ws) => {
-    console.log('connected')
-
-    //connection is up, let's add a simple simple event
-    ws.on('message', (message) => {
-
-        //log the received message and send it back to the client
-        console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
-    });
-
-    //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server');
-});
-
-app.use('/static', (req, res) => {
-    express.static(path.join(__dirname, 'static'))
-})
-app.get('/test', function (req, res) {
-  res.send('Hello World!')
-})
+// var stockfish = new Worker("stockfish.js");
+// console.log(stockfish.sendMessage());
+var a = STOCKFISH();
 
 //start our server
-server.listen(process.env.PORT || 5000, () => {
-    console.log(`Server started on port ${server.address().port} :)`);
-});
+// server.listen(process.env.PORT || 5000, () => {
+//     console.log(`Server started on port ${server.address().port} :)`);
+// });
